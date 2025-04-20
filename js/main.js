b076849074226675906cbf633a79be24120deca5
shennawy <ahmedshennawy997@gmail.com>
2025-04-20 19:01:52 +0200

    final st.

--- js/main.js
+++ js/main.js
@@ -5,7 +5,8 @@ let navbar = document.querySelector(".navbar");
 window.addEventListener("scroll", () => {
   upBtn.style.opacity = window.pageYOffset > 300 ? "1" : "0";
   navbar.style.backgroundColor =
-    window.pageYOffset > 300 ? "#000000b0" : "#151853";
+    window.pageYOffset > 300 ? "#000000b0" : "#000";
+  navbar.style.padding = window.pageYOffset > 300 ? "10px 0px" : "20px 0px";
 });
 
 upBtn.addEventListener("click", () => {
