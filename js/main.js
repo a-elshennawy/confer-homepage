fc35f0f947451b096b380007a096467c896a09fb
shennawy <ahmedshennawy997@gmail.com>
2025-04-20 05:54:21 +0200

    scroll animation

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 // upBtn
 let upBtn = document.querySelector(".upBtn");
+let navbar = document.querySelector(".navbar");
 
 window.addEventListener("scroll", () => {
   upBtn.style.opacity = window.pageYOffset > 300 ? "1" : "0";
+  navbar.style.backgroundColor =
+    window.pageYOffset > 300 ? "#000000b0" : "#151853";
 });
 
 upBtn.addEventListener("click", () => {
